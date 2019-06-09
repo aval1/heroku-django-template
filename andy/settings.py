f01e423cfe99c9671638b9149af7d301ec5cb57e
Andy Val <andyval4@gmail.com>
2019-06-09 04:32:11 -0400

    brains frying again

--- andy/settings.py
+++ andy/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'andy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': ['andy/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
